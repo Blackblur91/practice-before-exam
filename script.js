f5512c32f1fd7d7454845f6177fb9e6caa79ab7e
Blackblur91 <feketebalazs04@gmail.com>
2023-11-21 18:31:33 +0100

    test

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector("root").insertAdjacentHTML("beforeend", `
+<form>
+  <h1>Ez egy teszt felület vizsgához</h1>
+
+  <input type="text">
+
+  <textarea name="" id="" cols="30" rows="10">Ide kell írni az összes szöveget</textarea>
+
+  <button>KATT IDE</button>
+
+  <div>
+    <input type="checkbox">
+    <p>Ezt kell bepipálni majd</p>
+  </div>
+    </form>
+
+  </div>
+
+`)
